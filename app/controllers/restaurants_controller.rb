8de6175ad105cc903c518c9117ca3678e157dc1a
Alex Peattie <alexpeattie@gmail.com>
2014-01-13 11:34:45 +0000

    Add restaurants feature

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,16 @@
+class RestaurantsController < ApplicationController
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    Restaurant.create params[:restaurant].permit(:name, :description)
+    redirect_to '/restaurants'
+  end
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+end
